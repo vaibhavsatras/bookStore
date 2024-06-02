838cf23d5ba1220a8df4882e98552b12470dde74
vaibhavsatras <124855358+vaibhavsatras@users.noreply.github.com>
2024-06-02 17:37:25 +0530

    Update freeBooks.js

--- src/Components/freeBooks.js
+++ src/Components/freeBooks.js
@@ -74,8 +74,8 @@ function FreeBooks() {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             infinite: true,
             dots: true
           }
